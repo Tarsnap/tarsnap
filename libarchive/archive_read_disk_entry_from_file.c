ab4e68323bc66a7f10d674a62744ae27c27a6e74
Colin Percival <cperciva@tarsnap.com>
2015-06-27 12:31:51 -0700

    Avoid memory leak in setup_xattr error path
    If l?getxattr fails on the second call, the buffer we allocated to hold
    the extended attribute data was not being freed.

--- libarchive/archive_read_disk_entry_from_file.c
+++ libarchive/archive_read_disk_entry_from_file.c
@@ -336,6 +336,7 @@ setup_xattr(struct archive_read_disk *a,
 		size = getxattr(accpath, name, value, size);
 
 	if (size == -1) {
+		free(value);
 		archive_set_error(&a->archive, errno,
 		    "Couldn't read extended attribute");
 		return (ARCHIVE_WARN);
