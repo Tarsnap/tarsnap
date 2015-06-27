967cd659b16b269ea425e94f4dc142f72fe40104
Colin Percival <cperciva@tarsnap.com>
2015-06-27 15:26:14 -0700

    MFlibcperciva r44-48: Split network_buf.c.
    ------------------------------------------------------------------------
    r44 | cperciva | 2013-08-20 20:01:22 -0700 (Tue, 20 Aug 2013) | 2 lines
    
    Split network_buf.c into network_read.c and network_write.c.
    ------------------------------------------------------------------------
    r45 | cperciva | 2013-08-20 20:05:19 -0700 (Tue, 20 Aug 2013) | 3 lines
    
    Remove read-specific code from network_write.c and write-specific code from
    network_read.c.
    ------------------------------------------------------------------------
    r46 | cperciva | 2013-08-20 21:10:36 -0700 (Tue, 20 Aug 2013) | 3 lines
    
    Further clean up network_read/network_write, replacing unnecessarily
    general-purpose code with code specific to the two separate cases.
    ------------------------------------------------------------------------
    r47 | cperciva | 2013-08-20 21:17:38 -0700 (Tue, 20 Aug 2013) | 2 lines
    
    A few more fixups falling out of the buf -> {read, write} split.
    ------------------------------------------------------------------------
    r48 | cperciva | 2013-08-21 00:06:55 -0700 (Wed, 21 Aug 2013) | 5 lines
    
    Rework how we prevent SIGPIPE.  POSIX.1-2008 requires that MSG_NOSIGNAL be
    defined, so we use that as our primary strategy; for compatibilty with
    non-POSIX.1-2008 compliant systems, add POSIXFAIL_MSG_NOSIGNAL which, if
    defined, allows for blocking SIGPIPE if necessary.
    
    ------------------------------------------------------------------------

---
+++ libcperciva/network/network_write.c
@@ -0,0 +1,188 @@
+#include <sys/socket.h>
+
+#include <assert.h>
+#include <errno.h>
+#include <limits.h>
+#include <signal.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "events.h"
+#include "warnp.h"
+
+#include "network.h"
+
+/**
+ * POSIX.1-2008 requires that MSG_NOSIGNAL be defined as a flag for send(2)
+ * which has the effect of preventing SIGPIPE from being raised when writing
+ * to a descriptor which has been shut down.  Unfortunately there are some
+ * platforms which are not POSIX.1-2008 compliant; we provide a workaround
+ * (-DPOSIXFAIL_MSG_NOSIGNAL) which instead blocks the SIGPIPE signal on such
+ * platforms.
+ *
+ * (This workaround could be used automatically, but requiring that it be
+ * explicitly enabled helps to get platforms fixed.)
+ */
+#ifdef POSIXFAIL_MSG_NOSIGNAL
+#ifndef MSG_NOSIGNAL
+#define MSG_NOSIGNAL 0
+#endif
+#endif
+
+struct network_write_cookie {
+	int (*callback)(void *, ssize_t);
+	void * cookie;
+	int fd;
+	const uint8_t * buf;
+	size_t buflen;
+	size_t minlen;
+	size_t bufpos;
+};
+
+/* Invoke the callback, clean up, and return the callback's status. */
+static int
+docallback(struct network_write_cookie * C, ssize_t nbytes)
+{
+	int rc;
+
+	/* Invoke the callback. */
+	rc = (C->callback)(C->cookie, nbytes);
+
+	/* Clean up. */
+	free(C);
+
+	/* Return the callback's status. */
+	return (rc);
+}
+
+/* The socket is ready for reading/writing. */
+static int
+callback_buf(void * cookie)
+{
+	struct network_write_cookie * C = cookie;
+	size_t oplen;
+	ssize_t len;
+#if MSG_NOSIGNAL == 0
+	void (*oldsig)(int);
+#endif
+
+	/* If we don't have MSG_NOSIGNAL, catch SIGPIPE. */
+#if MSG_NOSIGNAL == 0
+	if ((oldsig = signal(SIGPIPE, SIG_IGN)) == SIG_ERR) {
+		warnp("signal(SIGPIPE)");
+		goto failed;
+	}
+#endif
+
+	/* Attempt to read/write data to/from the buffer. */
+	oplen = C->buflen - C->bufpos;
+	len = send(C->fd, C->buf + C->bufpos, oplen, MSG_NOSIGNAL);
+
+	/* We should never see a send length of zero. */
+	assert(len != 0);
+
+	/* If we set a SIGPIPE handler, restore the old one. */
+#if MSG_NOSIGNAL == 0
+	if (signal(SIGPIPE, oldsig) == SIG_ERR) {
+		warnp("signal(SIGPIPE)");
+		goto failed;
+	}
+#endif
+
+	/* Failure? */
+	if (len == -1) {
+		/* Was it really an error, or just a try-again? */
+		if ((errno == EAGAIN) ||
+		    (errno == EWOULDBLOCK) ||
+		    (errno == EINTR))
+			goto tryagain;
+
+		/* Something went wrong. */
+		goto failed;
+	}
+
+	/* We processed some data.  Do we need to keep going? */
+	if ((C->bufpos += len) < C->minlen)
+		goto tryagain;
+
+	/* Invoke the callback and return. */
+	return (docallback(C, C->bufpos));
+
+tryagain:
+	/* Reset the event. */
+	if (events_network_register(callback_buf, C, C->fd,
+	    EVENTS_NETWORK_OP_WRITE))
+		goto failed;
+
+	/* Callback was reset. */
+	return (0);
+
+failed:
+	/* Invoke the callback with a failure status and return. */
+	return (docallback(C, -1));
+}
+
+/**
+ * network_write(fd, buf, buflen, minwrite, callback, cookie):
+ * Asynchronously write up to ${buflen} bytes of data from ${buf} to ${fd}.
+ * When at least ${minwrite} bytes have been written or on error, invoke
+ * ${callback}(${cookie}, lenwrit), where lenwrit is -1 on error and the
+ * number of bytes written (between ${minwrite} and ${buflen} inclusive)
+ * otherwise.  Return a cookie which can be passed to network_write_cancel in
+ * order to cancel the write.
+ */
+void *
+network_write(int fd, const uint8_t * buf, size_t buflen, size_t minwrite,
+    int (* callback)(void *, ssize_t), void * cookie)
+{
+	struct network_write_cookie * C;
+
+	/* Make sure buflen is non-zero. */
+	assert(buflen != 0);
+
+	/* Sanity-check: # bytes must fit into a ssize_t. */
+	assert(buflen <= SSIZE_MAX);
+
+	/* Bake a cookie. */
+	if ((C = malloc(sizeof(struct network_write_cookie))) == NULL)
+		goto err0;
+	C->callback = callback;
+	C->cookie = cookie;
+	C->fd = fd;
+	C->buf = buf;
+	C->buflen = buflen;
+	C->minlen = minwrite;
+	C->bufpos = 0;
+
+	/* Register a callback for network readiness. */
+	if (events_network_register(callback_buf, C, C->fd,
+	    EVENTS_NETWORK_OP_WRITE))
+		goto err1;
+
+	/* Success! */
+	return (C);
+
+err1:
+	free(C);
+err0:
+	/* Failure! */
+	return (NULL);
+}
+
+/**
+ * network_write_cancel(cookie):
+ * Cancel the buffer write for which the cookie ${cookie} was returned by
+ * network_write.  Do not invoke the callback associated with the write.
+ */
+void
+network_write_cancel(void * cookie)
+{
+	struct network_write_cookie * C = cookie;
+
+	/* Kill the network event. */
+	events_network_cancel(C->fd, EVENTS_NETWORK_OP_WRITE);
+
+	/* Free the cookie. */
+	free(C);
+}
