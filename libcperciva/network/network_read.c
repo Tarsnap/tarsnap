a8c9eec230dfe47c39ddfa4ec5614db9eaaef5c3
Colin Percival <cperciva@tarsnap.com>
2015-06-27 15:26:14 -0700

    MFlibcperciva r44-48: Split network_buf.c.
    ------------------------------------------------------------------------
    r44 | cperciva | 2013-08-20 20:01:22 -0700 (Tue, 20 Aug 2013) | 2 lines
    
    Split network_buf.c into network_read.c and network_write.c.
    ------------------------------------------------------------------------
    r45 | cperciva | 2013-08-20 20:05:19 -0700 (Tue, 20 Aug 2013) | 3 lines
    
    Remove read-specific code from network_write.c and write-specific code from
    network_read.c.
    ------------------------------------------------------------------------
    r46 | cperciva | 2013-08-20 21:10:36 -0700 (Tue, 20 Aug 2013) | 3 lines
    
    Further clean up network_read/network_write, replacing unnecessarily
    general-purpose code with code specific to the two separate cases.
    ------------------------------------------------------------------------
    r47 | cperciva | 2013-08-20 21:17:38 -0700 (Tue, 20 Aug 2013) | 2 lines
    
    A few more fixups falling out of the buf -> {read, write} split.
    ------------------------------------------------------------------------
    r48 | cperciva | 2013-08-21 00:06:55 -0700 (Wed, 21 Aug 2013) | 5 lines
    
    Rework how we prevent SIGPIPE.  POSIX.1-2008 requires that MSG_NOSIGNAL be
    defined, so we use that as our primary strategy; for compatibilty with
    non-POSIX.1-2008 compliant systems, add POSIXFAIL_MSG_NOSIGNAL which, if
    defined, allows for blocking SIGPIPE if necessary.
    
    ------------------------------------------------------------------------

---
+++ libcperciva/network/network_read.c
@@ -0,0 +1,155 @@
+#include <sys/socket.h>
+
+#include <assert.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "events.h"
+#include "warnp.h"
+
+#include "network.h"
+
+struct network_read_cookie {
+	int (*callback)(void *, ssize_t);
+	void * cookie;
+	int fd;
+	uint8_t * buf;
+	size_t buflen;
+	size_t minlen;
+	size_t bufpos;
+};
+
+/* Invoke the callback, clean up, and return the callback's status. */
+static int
+docallback(struct network_read_cookie * C, ssize_t nbytes)
+{
+	int rc;
+
+	/* Invoke the callback. */
+	rc = (C->callback)(C->cookie, nbytes);
+
+	/* Clean up. */
+	free(C);
+
+	/* Return the callback's status. */
+	return (rc);
+}
+
+/* The socket is ready for reading/writing. */
+static int
+callback_buf(void * cookie)
+{
+	struct network_read_cookie * C = cookie;
+	size_t oplen;
+	ssize_t len;
+
+	/* Attempt to read/write data to/from the buffer. */
+	oplen = C->buflen - C->bufpos;
+	len = recv(C->fd, C->buf + C->bufpos, oplen, 0);
+
+	/* Failure? */
+	if (len == -1) {
+		/* Was it really an error, or just a try-again? */
+		if ((errno == EAGAIN) ||
+		    (errno == EWOULDBLOCK) ||
+		    (errno == EINTR))
+			goto tryagain;
+
+		/* Something went wrong. */
+		goto failed;
+	} else if (len == 0) {
+		/* The socket was shut down by the remote host. */
+		goto eof;
+	}
+
+	/* We processed some data.  Do we need to keep going? */
+	if ((C->bufpos += len) < C->minlen)
+		goto tryagain;
+
+	/* Invoke the callback and return. */
+	return (docallback(C, C->bufpos));
+
+tryagain:
+	/* Reset the event. */
+	if (events_network_register(callback_buf, C, C->fd,
+	    EVENTS_NETWORK_OP_READ))
+		goto failed;
+
+	/* Callback was reset. */
+	return (0);
+
+eof:
+	/* Invoke the callback with an EOF status and return. */
+	return (docallback(C, 0));
+
+failed:
+	/* Invoke the callback with a failure status and return. */
+	return (docallback(C, -1));
+}
+
+/**
+ * network_read(fd, buf, buflen, minread, callback, cookie):
+ * Asynchronously read up to ${buflen} bytes of data from ${fd} into ${buf}.
+ * When at least ${minread} bytes have been read or on error, invoke
+ * ${callback}(${cookie}, lenread), where lenread is 0 on EOF or -1 on error,
+ * and the number of bytes read (between ${minread} and ${buflen} inclusive)
+ * otherwise.  Return a cookie which can be passed to network_read_cancel in
+ * order to cancel the read.
+ */
+void *
+network_read(int fd, uint8_t * buf, size_t buflen, size_t minread,
+    int (* callback)(void *, ssize_t), void * cookie)
+{
+	struct network_read_cookie * C;
+
+	/* Make sure buflen is non-zero. */
+	assert(buflen != 0);
+
+	/* Sanity-check: # bytes must fit into a ssize_t. */
+	assert(buflen <= SSIZE_MAX);
+
+	/* Bake a cookie. */
+	if ((C = malloc(sizeof(struct network_read_cookie))) == NULL)
+		goto err0;
+	C->callback = callback;
+	C->cookie = cookie;
+	C->fd = fd;
+	C->buf = buf;
+	C->buflen = buflen;
+	C->minlen = minread;
+	C->bufpos = 0;
+
+	/* Register a callback for network readiness. */
+	if (events_network_register(callback_buf, C, C->fd,
+	    EVENTS_NETWORK_OP_READ))
+		goto err1;
+
+	/* Success! */
+	return (C);
+
+err1:
+	free(C);
+err0:
+	/* Failure! */
+	return (NULL);
+}
+
+/**
+ * network_read_cancel(cookie):
+ * Cancel the buffer read for which the cookie ${cookie} was returned by
+ * network_read.  Do not invoke the callback associated with the read.
+ */
+void
+network_read_cancel(void * cookie)
+{
+	struct network_read_cookie * C = cookie;
+
+	/* Kill the network event. */
+	events_network_cancel(C->fd, EVENTS_NETWORK_OP_READ);
+
+	/* Free the cookie. */
+	free(C);
+}
