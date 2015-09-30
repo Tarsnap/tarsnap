9c4eb088f7224613980653e46ff6566f505ace15
Graham Percival <gperciva@tarsnap.com>
2015-09-30 14:35:25 -0700

    Allow 'd' and 'print-stats' to ignore missing tapename
    In particular, this changes tarsnap_mode_d() and tarsnap_mode_print_stats() if
    bsdtar->option_keep_going is set.
    
    (bsdtar->option_keep_going will be enabled as a command-line option in the
    next commit)

--- tar/bsdtar.h
+++ tar/bsdtar.h
@@ -73,6 +73,7 @@ struct bsdtar {
 	char		  option_fast_read; /* --fast-read */
 	char		  option_honor_nodump; /* --nodump */
 	char		  option_interactive; /* -w */
+	char		  option_keep_going; /* --keep-going */
 	char		  option_no_owner; /* -o */
 	char		  option_no_subdirs; /* -n */
 	char		  option_null; /* --null */
