3ef1e79d6e5258517cb99107e80b0d415d00e085
Graham Percival <gperciva@tarsnap.com>
2015-11-09 23:58:08 -0800

    Replace @DATE@ in MAN pages

--- release-tools/mktarball.sh
+++ release-tools/mktarball.sh
@@ -6,11 +6,17 @@ if [ -z $VERSION ]; then
 	exit 1
 fi
 DESTDIR=tarsnap-autoconf-${VERSION}
+RELEASEDATE=`date "+%B %d, %Y"`
 
 # Copy bits in
 mkdir ${DESTDIR}
 cp COPYING Makefile.am actarsnap.m4 configure.ac tsserver .autom4te.cfg ${DESTDIR}/
 cp -R keygen keymgmt keyregen lib libarchive libcperciva misc pkg recrypt tar ${DESTDIR}/
+
+# Copy with substitution
+for MANPAGE in */*-man */*-mdoc */*-man.in */*-mdoc.in; do
+	sed -e "s/@DATE@/$RELEASEDATE/" < $MANPAGE > ${DESTDIR}/$MANPAGE
+done
 
 # Generate autotools files
 ( cd ${DESTDIR}
