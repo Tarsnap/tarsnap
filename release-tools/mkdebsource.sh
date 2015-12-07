033e756c79c0528301cbc46b0cbf63d7972a9e1f
Graham Percival <gperciva@tarsnap.com>
2015-12-06 16:02:35 -0800

    pkg: Add script to create debian packaging files

---
+++ release-tools/mkdebsource.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+BUILDDIR=/tmp/tarsnap-debian-source/
+
+# Handle arguments
+RELEASE_TARBALL=$1
+REVISION=$2
+if [ ! -f $RELEASE_TARBALL ] || [ -z $REVISION ]; then
+	echo "Usage: mkdebsource.sh RELEASE_TARBALL REVISION"
+	exit 1
+fi
+
+# This can only be run on a system with debuild
+if ! command -v debuild >/dev/null 2>&1; then
+	echo "This script requires debuild"
+	exit 1
+fi
+
+# Extract version from tarball name
+IFS='-.' read -r STR1 STR2 X Y Z STR3 <<EOF
+$(basename $RELEASE_TARBALL)
+EOF
+VERSION="$X.$Y.$Z"
+
+# Define variables to be used later (requires $VERSION)
+RELEASE_DIRNAME=tarsnap-autoconf-$VERSION
+RENAMED_TARBALL=tarsnap_$VERSION.orig.tar.gz
+
+# Copy (with renaming) release tarball to match debian's *required* format.
+mkdir -p $BUILDDIR
+cp $RELEASE_TARBALL $BUILDDIR/$RENAMED_TARBALL
+
+# Extract release tarball
+cd $BUILDDIR
+tar -xzf $RENAMED_TARBALL
+
+# Prepare source directory for debianizing
+cd $RELEASE_DIRNAME
+cp -r pkg/debian .
+
+# Debianize: create tarsnap_*.debian.tar.gz and tarsnap_*.dsc
+debuild -S -us -uc
+
+# Inform the user of the package location
+echo "\nDebian source package created in:"
+echo "    ${BUILDDIR}"
+
