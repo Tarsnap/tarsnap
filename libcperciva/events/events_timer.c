a912884e1d501ed8d68d129366663ce56002f8c3
Graham Percival <gperciva@tarsnap.com>
2016-06-19 21:32:42 -0700

    events_timer.c: explit conversion to time_t, suseconds_t

--- libcperciva/events/events_timer.c
+++ libcperciva/events/events_timer.c
@@ -99,8 +99,8 @@ events_timer_register_double(int (*func)(void *), void * cookie,
 	struct timeval tv;
 
 	/* Convert timeo to a struct timeval. */
-	tv.tv_sec = timeo;
-	tv.tv_usec = (timeo - tv.tv_sec) * 1000000.0;
+	tv.tv_sec = (time_t)timeo;
+	tv.tv_usec = (suseconds_t)((timeo - tv.tv_sec) * 1000000.0);
 
 	/* Schedule the timeout. */
 	return (events_timer_register(func, cookie, &tv));
