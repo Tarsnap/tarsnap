7bdd4955f8dfdc1061b7e314a8a655db2e046bed
Colin Percival <cperciva@tarsnap.com>
2017-07-28 14:24:37 -0700

    Merge libarchive 97f964e3
    This improves the handling of corrupt "ar" archives; prior to this
    commit it was possible to underflow an array in what appears to be
    an unexploitable way.

--- libarchive/archive_read_support_format_ar.c
+++ libarchive/archive_read_support_format_ar.c
@@ -251,8 +251,15 @@ archive_read_format_ar_read_header(struct archive_read *a,
 	 * and are not terminated in '/', so we don't trim anything
 	 * that starts with '/'.)
 	 */
-	if (filename[0] != '/' && *p == '/')
+	if (filename[0] != '/' && p > filename && *p == '/') {
 		*p = '\0';
+	}
+
+	if (p < filename) {
+		archive_set_error(&a->archive, ARCHIVE_ERRNO_MISC,
+		    "Found entry with empty filename");
+		return (ARCHIVE_FATAL);
+	}
 
 	/*
 	 * '//' is the GNU filename table.
