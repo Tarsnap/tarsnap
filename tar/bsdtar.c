d23e64a511fe17752326cc9f7af20d222e5aeb40
Graham Percival <gperciva@tarsnap.com>
2018-01-26 10:48:04 -0800

    Add global tarsnap_opt_uploadprogressbytes var
    This variable will be used in the next commit.

--- tar/bsdtar.c
+++ tar/bsdtar.c
@@ -92,6 +92,7 @@ int tarsnap_opt_humanize_numbers = 0;
 int tarsnap_opt_noisy_warnings = 0;
 uint64_t tarsnap_opt_checkpointbytes = (uint64_t)(-1);
 uint64_t tarsnap_opt_maxbytesout = (uint64_t)(-1);
+uint64_t tarsnap_opt_uploadprogressbytes = (uint64_t)(-1);
 
 /* Structure for holding a delayed option. */
 struct delayedopt {
