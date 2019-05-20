75826cf0536bcf05b9beed1e10d9ec0192f791d9
Graham Percival <gperciva@tarsnap.com>
2019-05-20 13:53:11 -0700

    Don't allow r or p to be zero

--- lib/crypto/crypto_scrypt.c
+++ lib/crypto/crypto_scrypt.c
@@ -65,6 +65,10 @@ _crypto_scrypt(const uint8_t * passwd, size_t passwdlen,
 	uint32_t i;
 
 	/* Sanity-check parameters. */
+	if ((r == 0) || (p == 0)) {
+		errno = EINVAL;
+		goto err0;
+	}
 #if SIZE_MAX > UINT32_MAX
 	if (buflen > (((uint64_t)(1) << 32) - 1) * 32) {
 		errno = EFBIG;
@@ -233,8 +237,8 @@ selectsmix(void)
  * crypto_scrypt(passwd, passwdlen, salt, saltlen, N, r, p, buf, buflen):
  * Compute scrypt(passwd[0 .. passwdlen - 1], salt[0 .. saltlen - 1], N, r,
  * p, buflen) and write the result into buf.  The parameters r, p, and buflen
- * must satisfy r * p < 2^30 and buflen <= (2^32 - 1) * 32.  The parameter N
- * must be a power of 2 greater than 1.
+ * must satisfy 0 < r * p < 2^30 and buflen <= (2^32 - 1) * 32.  The parameter
+ * N must be a power of 2 greater than 1.
  *
  * Return 0 on success; or -1 on error.
  */
