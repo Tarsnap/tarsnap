5aaf6f8a423fb7eaeb481fb1435a0ba337a40f25
Graham Percival <gperciva@tarsnap.com>
2019-05-20 13:53:11 -0700

    Don't allow r or p to be zero

--- lib/crypto/crypto_scrypt.h
+++ lib/crypto/crypto_scrypt.h
@@ -36,8 +36,8 @@
  * crypto_scrypt(passwd, passwdlen, salt, saltlen, N, r, p, buf, buflen):
  * Compute scrypt(passwd[0 .. passwdlen - 1], salt[0 .. saltlen - 1], N, r,
  * p, buflen) and write the result into buf.  The parameters r, p, and buflen
- * must satisfy r * p < 2^30 and buflen <= (2^32 - 1) * 32.  The parameter N
- * must be a power of 2 greater than 1.
+ * must satisfy 0 < r * p < 2^30 and buflen <= (2^32 - 1) * 32.  The parameter
+ * N must be a power of 2 greater than 1.
  *
  * Return 0 on success; or -1 on error.
  */
