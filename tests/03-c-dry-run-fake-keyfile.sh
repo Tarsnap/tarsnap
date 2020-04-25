d1fa5e8447fb833f8cafe50f92fcdb98b889f553
Graham Percival <graham@percival-music.ca>
2020-04-24 19:08:51 -0700

    tests: add -c --dry-run (with a fake keyfile)
    This does not require network connectivity.

---
+++ tests/03-c-dry-run-fake-keyfile.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+### Constants
+c_valgrind_min=1
+samplefile=${scriptdir}/test_tarsnap.sh
+out_stats_stderr=${s_basename}-output-stats.stderr
+keyfile=${scriptdir}/fake.keys
+cachedir=${s_basename}-cachedir
+init_cache_stderr=${s_basename}-cachedir.stderr
+
+scenario_cmd() {
+	# Create a cache directory.
+	setup_check_variables
+	${c_valgrind_cmd} ./tarsnap --no-default-config		\
+		--keyfile ${keyfile} --cachedir ${cachedir}	\
+		--initialize-cachedir				\
+		2> ${init_cache_stderr}
+	echo $? > ${c_exitfile}
+
+	setup_check_variables
+	grep -q "created for" ${init_cache_stderr}
+	echo $? > ${c_exitfile}
+
+	# Check -c --dry-run --print-stats.  The precise stats
+	# will vary based on the system, so we can't check those.
+	# (This uses more code than a --dry-run without any keyfile.)
+	setup_check_variables
+	${c_valgrind_cmd} ./tarsnap --no-default-config		\
+		--keyfile ${keyfile} --cachedir ${cachedir}	\
+		-c --dry-run --print-stats			\
+		${samplefile}					\
+		2> ${out_stats_stderr}
+	echo $? > ${c_exitfile}
+
+	setup_check_variables
+	grep -q "tarsnap: Removing leading" ${out_stats_stderr}
+	echo $? > ${c_exitfile}
+}
