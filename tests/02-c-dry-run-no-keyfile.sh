e7d1e03e2c80865e005106c6dcbce2984623dc66
Graham Percival <gperciva@tarsnap.com>
2020-06-02 19:07:56 -0700

    tests: add descriptions for each check

--- tests/02-c-dry-run-no-keyfile.sh
+++ tests/02-c-dry-run-no-keyfile.sh
@@ -8,7 +8,7 @@ out_stats_stderr=${s_basename}-output-stats.stderr
 scenario_cmd() {
 	# Check -c --dry-run --print-stats.  The precise stats
 	# will vary based on the system, so we can't check those.
-	setup_check_variables
+	setup_check_variables "check -c --dry-run"
 	${c_valgrind_cmd} ./tarsnap --no-default-config		\
 		-c --dry-run --print-stats			\
 		${samplefile}					\
@@ -16,12 +16,12 @@ scenario_cmd() {
 	echo $? > ${c_exitfile}
 
 	# Check expected warning message.
-	setup_check_variables
+	setup_check_variables "check -c --dry-run output"
 	grep -q "tarsnap: Removing leading" ${out_stats_stderr}
 	echo $? > ${c_exitfile}
 
 	# Check another expected warning message.
-	setup_check_variables
+	setup_check_variables "check -c --dry-run warning"
 	grep -q "(sizes may be slightly inaccurate)" ${out_stats_stderr}
 	echo $? > ${c_exitfile}
 }
