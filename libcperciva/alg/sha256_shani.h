8b3ce40b4ff6424a4bc421d3b9e6754d63a24ab3
Graham Percival <gperciva@tarsnap.com>
2020-06-10 09:05:39 -0700

    sha256: use POSIXFAIL_ABSTRACT_DECLARATOR
    This is needed in order to compile with gcc 4.2.1.

--- libcperciva/alg/sha256_shani.h
+++ libcperciva/alg/sha256_shani.h
@@ -10,7 +10,12 @@
  * instructions, and should only be used if CPUSUPPORT_X86_SHANI and _SSSE3
  * are defined and cpusupport_x86_shani() and _ssse3() return nonzero.
  */
+#ifdef POSIXFAIL_ABSTRACT_DECLARATOR
+void SHA256_Transform_shani(uint32_t state[static restrict 8],
+    const uint8_t block[static restrict 64]);
+#else
 void SHA256_Transform_shani(uint32_t[static restrict 8],
     const uint8_t[static restrict 64]);
+#endif
 
 #endif /* !_SHA256_SHANI_H_ */
