6ace77a496c83cc13ecb7626a86670d4a4766094
Alexandro Sanchez <alexandro@phi.nz>
2021-02-16 11:08:55 +0100

    Fix missing #endif

--- tar/util.c
+++ tar/util.c
@@ -43,6 +43,7 @@ __FBSDID("$FreeBSD: src/usr.bin/tar/util.c,v 1.23 2008/12/15 06:00:25 kientzle E
 #ifdef MAJOR_IN_SYSMACROS
 #include <sys/sysmacros.h>
 #endif
+#endif
 #include <ctype.h>
 #ifdef HAVE_ERRNO_H
 #include <errno.h>
