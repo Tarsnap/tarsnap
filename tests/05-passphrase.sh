fa4503061009a4d2086be4f972c4e031a9ef5b84
Graham Percival <gperciva@tarsnap.com>
2021-05-23 10:32:22 -0700

    tests: adjust for --passphrase method:arg
    This includes switching --passphrase-stdin to
    --passphrase dev:stdin-once, and adding more --passphrase method:arg tests.

--- tests/05-passphrase.sh
+++ tests/05-passphrase.sh
@@ -13,7 +13,7 @@ scenario_cmd() {
 		--keyfile ${keyfile}				\
 		--cachedir ${s_basename}-cachedir-${s_count}	\
 		--initialize-cachedir				\
-		--passphrase-stdin				\
+		--passphrase dev:stdin-once			\
 		2> ${s_basename}-${s_count}.stderr
 	expected_exitcode 1 $? > ${c_exitfile}
 
@@ -24,7 +24,29 @@ scenario_cmd() {
 		--keyfile ${keyfile}				\
 		--cachedir ${s_basename}-cachedir-${s_count}	\
 		--initialize-cachedir				\
-		--passphrase-stdin				\
+		--passphrase dev:stdin-once			\
+		2> ${s_basename}-${s_count}.stderr
+	echo $? > ${c_exitfile}
+
+	# Create a cache directory with the correct passphrase in env.
+	setup_check_variables "check --initialize-cachedir, passphrase env"
+	PASSENV="hunter2"					\
+		${c_valgrind_cmd} ./tarsnap --no-default-config	\
+		--keyfile ${keyfile}				\
+		--cachedir ${s_basename}-cachedir-${s_count}	\
+		--initialize-cachedir				\
+		--passphrase env:PASSENV			\
+		2> ${s_basename}-${s_count}.stderr
+	echo $? > ${c_exitfile}
+
+	# Create a cache directory with the correct passphrase in a file.
+	setup_check_variables "check --initialize-cachedir, passphrase file"
+	printf "hunter2\n" > ${s_basename}-passphrase.txt
+	${c_valgrind_cmd} ./tarsnap --no-default-config		\
+		--keyfile ${keyfile}				\
+		--cachedir ${s_basename}-cachedir-${s_count}	\
+		--initialize-cachedir				\
+		--passphrase file:${s_basename}-passphrase.txt	\
 		2> ${s_basename}-${s_count}.stderr
 	echo $? > ${c_exitfile}
 }
