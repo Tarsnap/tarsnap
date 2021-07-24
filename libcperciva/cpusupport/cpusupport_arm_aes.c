8bebadb5072782ba620fbd31d7a47cda4ef568c5
Graham Percival <gperciva@tarsnap.com>
2021-07-24 09:57:47 -0700

    cpusupport: add runtime checks for ARM on FreeBSD
    The AT_HWCAP and AT_HWCAP2 flags are defined in <sys/elf_common.h>, and
    documented in elf_aux_info(3).
    
    The actual values of HWCAP_foo and HWCAP2_foo flags depend on the
    machine is arm or arm64:
    https://github.com/freebsd/freebsd-src/blob/master/sys/arm/include/elf.h
    https://github.com/freebsd/freebsd-src/blob/master/sys/arm64/include/elf.h
    <sys/auvx.h> itself includes <machine/elf.h>, so we don't need to
    include it directly.

--- libcperciva/cpusupport/cpusupport_arm_aes.c
+++ libcperciva/cpusupport/cpusupport_arm_aes.c
@@ -15,6 +15,10 @@
 #endif /* __arm__ */
 #endif /* CPUSUPPORT_HWCAP_GETAUXVAL */
 
+#if defined(CPUSUPPORT_HWCAP_ELF_AUX_INFO)
+#include <sys/auxv.h>
+#endif /* CPUSUPPORT_HWCAP_ELF_AUX_INFO */
+
 CPUSUPPORT_FEATURE_DECL(arm, aes)
 {
 	int supported = 0;
@@ -31,6 +35,20 @@ CPUSUPPORT_FEATURE_DECL(arm, aes)
 	supported = (capabilities & HWCAP2_AES) ? 1 : 0;
 #endif
 #endif /* CPUSUPPORT_HWCAP_GETAUXVAL */
+
+#if defined(CPUSUPPORT_HWCAP_ELF_AUX_INFO)
+	unsigned long capabilities;
+
+#if defined(__aarch64__)
+	if (elf_aux_info(AT_HWCAP, &capabilities, sizeof(unsigned long)))
+		return (0);
+	supported = (capabilities & HWCAP_AES) ? 1 : 0;
+#else
+	if (elf_aux_info(AT_HWCAP2, &capabilities, sizeof(unsigned long)))
+		return (0);
+	supported = (capabilities & HWCAP2_AES) ? 1 : 0;
+#endif
+#endif /* CPUSUPPORT_HWCAP_ELF_AUX_INFO */
 #endif /* CPUSUPPORT_ARM_AES */
 
 	/* Return the supported status. */
