09a9d2ac26ef9797d4f323b8ab7accc57c73edd0
Graham Percival <gperciva@tarsnap.com>
2021-11-03 21:26:52 -0700

    passphrase_entry: fix API comment
    This should have been part of
        2020-10-10 passphrase_entry: add
        11bae08ecf3baf7ede87eb1dda94c9ac033a8393

--- lib/util/passphrase_entry.h
+++ lib/util/passphrase_entry.h
@@ -24,8 +24,9 @@ int passphrase_entry_parse(const char *, enum passphrase_entry *,
  * Use ${entry_method} to read a passphrase and return it as a malloced
  * NUL-terminated string via ${passwd}.  If ${entry_method} is
  * PASSPHRASE_TTY_STDIN and ${once} is zero, ask for the passphrase twice;
- * otherwise ask for it once.  Use ${prompt} for the first prompt, and
- * ${confirmprompt} for the second prompt (if applicable).
+ * otherwise ask for it once.  If reading from a terminal, use ${prompt} for
+ * the first prompt, and ${confirmprompt} for the second prompt (if
+ * applicable); otherwise do not print any prompts.
  */
 int passphrase_entry_readpass(char **, enum passphrase_entry, const char *,
     const char *, const char *, int);
