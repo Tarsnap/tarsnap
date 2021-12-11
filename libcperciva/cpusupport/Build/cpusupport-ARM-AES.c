b613280475992da2f4b21d7c0e49bcea8a954daf
Graham Percival <gperciva@tarsnap.com>
2021-12-10 19:32:31 -0800

    cpusupport: improve check for ARM-AES on arm32
    This should have been part of
        2021-05-30 cpusupport: ARM-AES compile-time detection
        ce48bbfe260975275e01e6ba37208ce723aefebe
    
    Some compilers enable vaeseq_u8 but not vdupq_laneq_u32.

--- libcperciva/cpusupport/Build/cpusupport-ARM-AES.c
+++ libcperciva/cpusupport/Build/cpusupport-ARM-AES.c
@@ -8,11 +8,14 @@ main(void)
 	uint8x16_t data;
 	uint8x16_t key = {0};
 	uint8x16_t output;
+	uint32x4_t lanes = {0};
 	uint8_t arr[16] = {0};
 
 	data = vld1q_u8(arr);
 	output = vaeseq_u8(data, key);
 	(void)output; /* UNUSED */
+
+	lanes = vdupq_laneq_u32(lanes, 0);
 
 	return (0);
 }
