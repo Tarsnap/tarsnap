caa3dcbafcde70c14d77a8b0845d898cdb84477b
Graham Percival <gperciva@tarsnap.com>
2025-03-05 13:18:05 -0800

    libarchive: set some pointers to NULL
    Reported by:	Ted Unangst
    Bug bounty:	$2

--- tar/tree.c
+++ tar/tree.c
@@ -297,6 +297,7 @@ tree_open(const char *path, int noatime)
 		abort();
 	memset(t, 0, sizeof(*t));
 	t->noatime = noatime;
+	t->buff = NULL;
 	tree_append(t, path, strlen(path));
 #ifdef HAVE_FCHDIR
 	t->initialDirFd = open(".", O_RDONLY);
