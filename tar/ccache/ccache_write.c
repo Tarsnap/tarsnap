d706a0fc74fc978c224865c53380bf69a078e64b
Graham Percival <gperciva@tarsnap.com>
2026-08-13 20:48:23 -0700

    ccache_write: ensure the cache direntry is durable
    Without this, a crash after rename() but before the directory is synced
    could leave the cache directory listing containing neither the old nor
    the new cache file.

--- tar/ccache/ccache_write.c
+++ tar/ccache/ccache_write.c
@@ -12,6 +12,7 @@
 
 #include "asprintf.h"
 #include "ccache_internal.h"
+#include "dirutil.h"
 #include "fileutil.h"
 #include "multitape_internal.h"
 #include "patricia.h"
@@ -292,6 +293,12 @@ ccache_write(CCACHE * cache, const char * path)
 	/* Move the new cache file into place. */
 	if (rename(W.s, s_old)) {
 		warnp("rename(%s, %s)", W.s, s_old);
+		free(s_old);
+		goto err1;
+	}
+
+	/* Make sure the rename is durable. */
+	if (dirutil_fsyncdir(path)) {
 		free(s_old);
 		goto err1;
 	}
