42d85e2604e5f1c27a69ffcca440a240457f7d6f
Graham Percival <gperciva@tarsnap.com>
2026-08-24 10:07:27 -0700

    archive_multitape: fix read path
    - add a warning message
    - document why we don't need any clean-up

--- tar/glue/archive_multitape.c
+++ tar/glue/archive_multitape.c
@@ -147,8 +147,17 @@ archive_read_open_multitape(struct archive * a, uint64_t machinenum,
 		goto err0;
 	}
 
-	if (archive_read_open2(a, d, NULL, read_read, read_skip, read_close))
+	if (archive_read_open2(a, d, NULL, read_read, read_skip, read_close)) {
+		archive_set_error(a, errno, "Error opening libarchive archive");
+		/*
+		 * We cannot call readtape_close(d) right now, because as long
+		 * as we provide read_read and no opener to
+		 * archive_read_open2(), it still sets the callbacks even if it
+		 * fails.  Later in the error path, libarchive code will call
+		 * read_close(), so no need to do that here.
+		 */
 		goto err0;
+	}
 
 	/* Success! */
 	return (d);
