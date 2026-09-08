b63c4deb38401ec453ac3d56c6ef9a9eef260ab2
Graham Percival <gperciva@tarsnap.com>
2026-09-08 14:44:55 -0700

    archive_multitape: fix error path in _write_open_
    This fixes a use-after-free bug.
    
    If archive_write_open() returned an error:
      1. writetape_free(d) was called by archive_write_open_multitape(), then
      2. archive_write_finish(a) was called by tarsnap_mode_c(), which called
      3. write_close(a, d) as a callback [*], which called
      4. writetape_close(d) on the already-freed (d).
    
    [*] archive_write_open() registers callback functions before any
        possible failure, and does not de-register them on error.
    
    Historical info
    ---------------
    
    Way back in:
        2008-04-25 Tarsnap 1.0.0
        41f175dbc884f04e36a98811188e1100bb4dbfae
    
    glue/archive_multitape.c contained:
    - archive_read_open_multitape(), which did not call readtape_close(d) upon
      a failure in archive_read_open2()
    - archive_write_open_multitape(), which called writetape_free(d) upon
      a failure in archive_write_open()
    
    The same is true now (other than that file moving into tar/glue/).
    
    The bug was introduced in:
        2015-09-02 tarsnap_mode_c(): return from errors
        bf7e46b17aed59128a0b00e7f997810e4d541c01
    when I added an error path for a failure in
    archive_write_open_multitape(), which called archive_write_finish().
    
    Reported by:	chengdazhi
    Bug bounty:	???

--- tar/glue/archive_multitape.c
+++ tar/glue/archive_multitape.c
@@ -197,7 +197,17 @@ archive_write_open_multitape(struct archive * a, uint64_t machinenum,
 	}
 
 	if (archive_write_open(a, d, NULL, write_write, write_close)) {
-		writetape_free(d);
+		archive_set_error(a, errno,
+		    "Error creating new libarchive archive");
+		/*
+		 * We cannot call writetape_free(d) right now, because even if
+		 * archive_write_open() fails, it still sets the callbacks.
+		 * Later in the error path, libarchive code will call
+		 * write_close(), which will then call writetape_close(), so
+		 * that function needs to know that we did not properly
+		 * initialize the archive.
+		 */
+		writetape_set_deferred_free(d);
 		goto err0;
 	}
 
